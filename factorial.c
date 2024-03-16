33a0803fdea36a9f5c0e7dd8b4918e67323cf5ea
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-16 14:12:36 +0000

    fact

--- factorial.c
+++ factorial.c
@@ -1,17 +1,15 @@
-#include<stdio.h>
-int main()
-{
-    int n;
-    printf("Enter a number:");
-    scanf("%d",&n);
-    printf("\n");
-    printf("%d! = ",n);
-    int factorial = 1;
-    for(int i=1; i<=n; i++)
-    {
-        factorial = factorial * i;
-    }
-    printf("%d",factorial);
-    printf("\n");
-    return 0;
+#include <stdio.h>
+int main() {
+  int n;
+  printf("Enter a number:");
+  scanf("%d", &n);
+  printf("\n");
+  printf("%d! = ", n);
+  int factorial = 1;
+  for (int i = 1; i <= n; i++) {
+    factorial = factorial * i;
+  }
+  printf("%d", factorial);
+  printf("\n");
+  return 0;
 }
