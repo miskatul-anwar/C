2f75ce4c8d153bbb725f4e4e52c986eef33eb0bc
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-18 17:17:25 +0000

    Mon Mar 18 05:17:25 PM UTC 2024

--- Algorithms/binarysearch.c
+++ Algorithms/binarysearch.c
@@ -3,56 +3,58 @@
 int linear_search(int search_value, int array[], int size);
 int binary_search(int array[], int size, int search_value);
 
-int main() {
-    int search_value;
-    int array[7] = {1, 5, 6, 23, 33, 67, 67};
-    int size = 7; // Size of the array
-    printf("Enter the value you need to search: ");
-    scanf("%d", &search_value);
+int main(void) {
+  int search_value;
+  int array[7] = {1, 5, 6, 23, 33, 67, 67};
+  int size = 7; // Size of the array
+  printf("Enter the value you need to search: ");
+  scanf("%d", &search_value);
 
-    // Using linear search
-    linear_search(search_value, array, size);
+  // Using linear search
+  linear_search(search_value, array, size);
 
-    // Using binary search
-    int binary_result = binary_search(array, size, search_value);
-    if (binary_result != -1) {
-        printf("Searched value %d found at %dth position using binary search.\n", search_value, binary_result + 1);
-    } else {
-        printf("Value is not available using binary search.\n");
-    }
+  // Using binary search
+  int binary_result = binary_search(array, size, search_value);
+  if (binary_result != -1) {
+    printf("Searched value %d found at %dth position using binary search.\n",
+           search_value, binary_result + 1);
+  } else {
+    printf("Value is not available using binary search.\n");
+  }
 
-    return 0;
+  return 0;
 }
 
 int linear_search(int search_value, int array[], int size) {
-    int found = 0; // Variable to track if value is found
+  int found = 0; // Variable to track if value is found
 
-    for (int i = 0; i < size; i++) {
-        if (search_value == array[i]) {
-            printf("Searched value %d found at %dth position using linear search.\n", search_value, i + 1);
-            found = 1; // Set found to 1 if value is found
-        }
+  for (int i = 0; i < size; i++) {
+    if (search_value == array[i]) {
+      printf("Searched value %d found at %dth position using linear search.\n",
+             search_value, i + 1);
+      found = 1; // Set found to 1 if value is found
     }
+  }
 
-    if (!found) { // If value is not found after the loop
-        printf("Value is not available using linear search.\n");
-    }
+  if (!found) { // If value is not found after the loop
+    printf("Value is not available using linear search.\n");
+  }
 
-    return 0;
+  return 0;
 }
 
 int binary_search(int array[], int size, int search_value) {
-    int low = 0, mid, high = size - 1;
-    while (low <= high) {
-        mid = (low + high) / 2;
-        if (array[mid] == search_value) {
-            return mid;
-        }
-        if (array[mid] < search_value) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
+  int low = 0, mid, high = size - 1;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (array[mid] == search_value) {
+      return mid;
     }
-    return -1; // Return -1 if the element is not found
+    if (array[mid] < search_value) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1; // Return -1 if the element is not found
 }
--- Algorithms/bubblesort.cpp
+++ Algorithms/bubblesort.cpp
@@ -1,46 +1,47 @@
 #include "iostream"
 #include "stdlib.h"
 using namespace std;
-//simple bubble sort algorithm 
-int main(void){
+// simple bubble sort algorithm
+int main(void) {
   int x, i, count;
-  cout<<"Enter a number: ";cin>>x;
-  int *array =(int*)malloc(x*sizeof(int*));
-  i=0;
-  for(;;){
-    if(i<x){
-      cout<<"index["<<i<<"] = ";
-      cin>>*(array+i);
-    }else{
+  cout << "Enter a number: ";
+  cin >> x;
+  int *array = (int *)malloc(x * sizeof(int *));
+  i = 0;
+  for (;;) {
+    if (i < x) {
+      cout << "index[" << i << "] = ";
+      cin >> *(array + i);
+    } else {
       break;
     }
     i++;
   }
-  cout<<endl;
-  cout<<"| ";
-  for(i=0;i<x;i++){
-    cout<<*(array+i)<<" | ";
+  cout << endl;
+  cout << "| ";
+  for (i = 0; i < x; i++) {
+    cout << *(array + i) << " | ";
   }
-  cout<<endl;
-  
+  cout << endl;
+
   count = 1;
-  while(count<x){
-    for(i=0;i<x-count;i++){
-      if(*(array+i)>*(array+i+1)){
-        int temp = *(array+i);
-        *(array+i) = *(array+i+1);
-        *(array+i+1) = temp;
-      }else{
+  while (count < x) {
+    for (i = 0; i < x - count; i++) {
+      if (*(array + i) > *(array + i + 1)) {
+        int temp = *(array + i);
+        *(array + i) = *(array + i + 1);
+        *(array + i + 1) = temp;
+      } else {
         continue;
       }
     }
     count++;
   }
-  cout<<endl<<"| ";
-  for(i=0;i<x;i++){
-    cout<<*(array+i)<<" | ";
+  cout << endl << "| ";
+  for (i = 0; i < x; i++) {
+    cout << *(array + i) << " | ";
   }
-  cout<<endl;
+  cout << endl;
   free(array);
   return 0;
 }
