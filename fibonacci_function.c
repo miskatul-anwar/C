7038e9a09e929c68928f6d3d0b1b4467544f709c
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-18 13:25:23 +0000

    Mon Mar 18 01:25:23 PM UTC 2024

--- fibonacci_function.c
+++ fibonacci_function.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 
 int fibonacci(int n);
-int main(void)
-{
+int main(void) {
   int nth_term;
   printf("Enter a number:");
   scanf("%d", &nth_term);
   fibonacci(nth_term);
 }
-int fibonacci(int n)
-{
+int fibonacci(int n) {
   int sum;
-  if(n<=1)
-  {
+  if (n <= 1) {
     return n;
+  } else {
+    sum = fibonacci(n - 1) + fibonacci(n - 2);
   }
-  else 
-  {
-    sum = fibonacci(n-1)+fibonacci(n-2);
-  
-  }
-  printf("%d",sum);
+  printf("%d", sum);
 }
