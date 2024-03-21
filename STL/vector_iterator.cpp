2f72b1a01e18f6f2dae5382e173a9b21184cb706
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-21 12:42:46 +0000

    Thu Mar 21 12:42:46 PM UTC 2024

--- STL/vector_iterator.cpp
+++ STL/vector_iterator.cpp
@@ -5,6 +5,11 @@ int main(void) {
   vector<int> v(10, 12);
   // This is practice for iterators ➿
   for (vector<int>::iterator j = v.begin(); j != v.end(); j++) {
-    cout << *j << endl;
+    cout << *j << " ";
   }
+  cout << endl;
+  for (vector<int>::iterator j = v.end(); j != v.begin(); j--) {
+    cout << *j << " ";
+  }
+  return 0;
 }
