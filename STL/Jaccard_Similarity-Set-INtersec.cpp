4770fada6243e1058983128501a35288b3fe72d4
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-24 08:38:03 +0000

    Sun Mar 24 08:38:03 AM UTC 2024

---
+++ STL/Jaccard_Similarity-Set-INtersec.cpp
@@ -0,0 +1,44 @@
+#include "bits/stdc++.h"
+using namespace std;
+int main(void) {
+  string doc_1 = "Data is the new oil of digital economy",
+         doc_2 = "Data is a new oil";
+  string eachword1, eachword2;
+  list<string> l1, l2, tmp1, tmp2;
+  stringstream s1(doc_1), s2(doc_2);
+  while (s1 >> eachword1) {
+    l1.push_back(eachword1);
+  }
+  while (s2 >> eachword2) {
+    l2.push_back(eachword2);
+  }
+  cout << "Original list 1: ";
+  for (auto x : l1) {
+    tmp1.push_back(x);
+    cout << x << " ";
+  }
+  cout << endl;
+  cout << "Original list 2: ";
+  for (auto x : l2) {
+    tmp2.push_back(x);
+    cout << x << " ";
+  }
+  cout << endl;
+
+  // union of the lists
+  l1.merge(l2);
+  l1.sort();
+  l1.unique();
+  int b = l1.size();
+
+  // intersection of the lists
+  tmp1.sort(), tmp2.sort();
+  list<string> common_list;
+  set_intersection(tmp1.begin(), tmp1.end(), tmp2.begin(), tmp2.end(),
+                   back_inserter(common_list));
+  int a = common_list.size();
+  // The Jaccard similarity of these two documents is estimated as follows
+  float jaccard = (float)a / b;
+  cout << "Jaccard similarity: " << jaccard << endl;
+  return 0;
+}
