38900ca1cea295a4cb0d5113c2bc6eede42f4ab8
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-25 14:10:04 +0000

    Mon Mar 25 02:10:04 PM UTC 2024

--- STL/smc.cpp
+++ STL/smc.cpp
@@ -1,4 +1,34 @@
 #include <bits/stdc++.h>
+#include <sstream>
+using namespace std;
 int main(void) {
-  // I'll Finish the code Later 😃
+  int count = 0;
+  float smc;
+  string x, y, eachword1, eachword2;
+  list<string> l1, l2;
+  cout << "x"
+       << " = ";
+  cin >> x;
+  cout << "y"
+       << " = ";
+  cin >> y;
+  cout << endl;
+  stringstream s1(x), s2(y);
+  while (s1 >> eachword1) {
+    l1.push_back(eachword1);
+  }
+  while (s2 >> eachword2) {
+    l2.push_back(eachword2);
+  }
+  count = 0;
+  for (auto i : l1) {
+    for (auto j : l2) {
+      if (i == j) {
+        count++;
+      }
+    }
+  }
+  smc = (float)count / l1.size();
+  cout << "Similarity: " << smc << endl;
+  return 0;
 }
