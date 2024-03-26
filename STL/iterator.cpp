dc2d687c4523fd676d5ce9345130665e24ddbf52
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-26 14:02:34 +0000

    Tue Mar 26 02:02:34 PM UTC 2024

---
+++ STL/iterator.cpp
@@ -0,0 +1,9 @@
+#include "bits/stdc++.h"
+using namespace std;
+int main(int argc, char **argv) {
+  int n = atoi(argv[1]);
+  for (auto i = 0; i < n; ++i) {
+    cout << i << endl;
+  }
+  return 0;
+}
