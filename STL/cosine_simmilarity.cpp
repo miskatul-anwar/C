9f8c526bc0f9e1135a298fa6d0172cf1145ff067
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-04-02 17:43:18 +0600

    Tue Apr  2 05:43:18 PM +06 2024

--- STL/cosine_simmilarity.cpp
+++ STL/cosine_simmilarity.cpp
@@ -2,28 +2,24 @@
 using namespace std;
 
 double cosineSimilarity(const string &str1, const string &str2) {
-  vector<int> vec1(256, 0);
-  vector<int> vec2(256, 0);
+  vector<int> vec1(256, 0), vec2(256, 0);
+  double dotProduct = 0, magnitude1 = 0, magnitude2 = 0;
 
-  for (char c : str1)
+  for (char c : str1) {
     vec1[c]++;
+  }
 
-  for (char c : str2)
+  for (char c : str2) {
     vec2[c]++;
+  }
 
-  double dotProduct = 0;
-  double magnitude1 = 0;
-  double magnitude2 = 0;
   for (int i = 0; i < 256; i++) {
     dotProduct += vec1[i] * vec2[i];
     magnitude1 += vec1[i] * vec1[i];
     magnitude2 += vec2[i] * vec2[i];
   }
 
-  magnitude1 = sqrt(magnitude1);
-  magnitude2 = sqrt(magnitude2);
-
-  double cosineSimilarity = dotProduct / (magnitude1 * magnitude2);
+  double cosineSimilarity = dotProduct / (sqrt(magnitude1 * magnitude2));
 
   return cosineSimilarity;
 }
