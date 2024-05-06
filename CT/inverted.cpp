fe007efd0480ed1582b7b775f28d9a1a242c91dd
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-05-06 19:04:22 +0600

    Mon May  6 07:04:22 PM +06 2024

--- CT/inverted.cpp
+++ CT/inverted.cpp
@@ -1,13 +1,19 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+int invert(int x, int p, int n);
+
 int main(void) {
-  int x = 21, p = 3, ans = 0;
+  int x = 21, p = 3;
   string size = to_string(x);
   int n = size.size();
-  for (int i = p - 1; i < n; i++) {
-    ans = x ^ (1 << i);
-  }
-  cout << ans << endl;
+  cout << invert(x, p, n) << endl;
   return 0;
+}
+
+int invert(int x, int p, int n) {
+  for (int i = p - 1; i < n + p - 1; i++) {
+    x = x ^ (1 << i);
+  }
+  return x;
 }
