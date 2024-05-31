f0aa636bebfccc04ebd9e82c4680808be6c95e24
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-05-31 08:09:04 +0600

    hi

--- STL/Jaccard_Similarity.cpp
+++ STL/Jaccard_Similarity.cpp
@@ -1,49 +1,54 @@
-#include "bits/stdc++.h"
-using namespace std;
-int main(void) {
-  string doc_1 = "Data is the new oil of digital economy",
-         doc_2 = "Data is a new oil";
-  string eachword1, eachword2;
-  list<string> l1, l2, tmp1, tmp2;
-  stringstream s1(doc_1), s2(doc_2);
-  while (s1 >> eachword1) {
-    l1.push_back(eachword1);
-  }
-  while (s2 >> eachword2) {
-    l2.push_back(eachword2);
-  }
-  cout << "Original list 1: ";
-  for (auto x : l1) {
-    tmp1.push_back(x);
-    cout << x << " ";
-  }
-  cout << endl;
-  cout << "Original list 2: ";
-  for (auto x : l2) {
-    tmp2.push_back(x);
-    cout << x << " ";
-  }
-  cout << endl;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-  // union of the lists
-  l1.merge(l2);
-  l1.sort();
-  l1.unique();
-  int b = l1.size();
+public
+class Main {
+public
+  static void main(String[] args) {
+    String doc1 = "Data is the new oil of digital economy",
+           doc2 = "Data is a new oil";
+    List<String> l1, l2, tmp1, tmp2;
 
-  // intersection of the lists
-  int count = 0;
-  tmp1.sort(), tmp2.sort();
-  for (auto x : tmp1) {
-    for (auto y : tmp2) {
-      if (x == y) {
+    l1 = new ArrayList<>();
+    l2 = new ArrayList<>();
+
+    for (String word : doc1.split(" ")) {
+      l1.add(word);
+    }
+
+    for (String word : doc2.split(" ")) {
+      l2.add(word);
+    }
+
+    System.out.println("Original list 1: " + String.join(" ", l1));
+    System.out.println("Original list 2: " + String.join(" ", l2));
+
+    // union of the lists
+    l1.addAll(l2);
+    Collections.sort(l1);
+    l1 = new ArrayList<>(new java.util.LinkedHashSet<>(l1));
+    int b = l1.size();
+
+    // intersection of the lists
+    tmp1 = new ArrayList<>(l1);
+    tmp2 = new ArrayList<>(l2);
+
+    Collections.sort(tmp1);
+    Collections.sort(tmp2);
+
+    int count = 0;
+    for (String word : tmp1) {
+      if (tmp2.contains(word)) {
         count++;
+        tmp2.remove(new Integer(tmp2.indexOf(word)));
       }
     }
+
+    int a = count;
+
+    // The Jaccard similarity of these two documents is estimated as follows
+    float jaccard = (float)a / b;
+    System.out.println("Jaccard similarity: " + jaccard);
   }
-  int a = count;
-  // The Jaccard similarity of these two documents is estimated as follows
-  float jaccard = (float)a / b;
-  cout << "Jaccard similarity: " << jaccard << endl;
-  return 0;
 }
